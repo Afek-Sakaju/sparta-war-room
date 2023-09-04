72dccf320682dcf84349cb9fbfae91f0d25fe20a
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-05 00:56:23 +0300

    removed unnecessary interface IOptions

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,3 @@
 export type { User } from './users-interface';
 export type { Tactic } from './tactics-interface';
 export type { Announcement } from './announcements-interface';
-export type { IOptions } from './mongoose-options-interface';
--- src/interfaces/mongoose-options-interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IOptions {
-  bufferCommands: boolean;
-  bufferTimeoutMS: number;
-}
