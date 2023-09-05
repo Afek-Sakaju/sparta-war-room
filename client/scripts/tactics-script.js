8b897d230e4eb2cfbec04f1f4ca9c4afb566975f
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-05 22:55:14 +0300

    changed linebreaks in the addLineBreaksToText usage of the tactics page to 2 instead of 1

--- client/scripts/tactics-script.js
+++ client/scripts/tactics-script.js
@@ -33,7 +33,7 @@ window.onload = async () => {
     const tacticDescription = document.createElement('p');
     tacticDescription.classList.add('tactic-text');
 
-    const replacedInformation = addLineBreaksToText(information);
+    const replacedInformation = addLineBreaksToText(information, 2);
     tacticDescription.textContent = replacedInformation;
     tacticWrapper.appendChild(tacticDescription);
 
