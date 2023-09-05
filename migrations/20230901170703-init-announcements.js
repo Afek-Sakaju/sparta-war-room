773fdb9125c60bbf4f3ae01bc845e554e5ea72b4
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-05 23:21:08 +0300

    improved content even more now all the content refers to the readers as spartans

--- migrations/20230901170703-init-announcements.js
+++ migrations/20230901170703-init-announcements.js
@@ -7,7 +7,7 @@ module.exports = {
       {
         title: 'Call to Adventure - Expedition to Mount Olympus!',
         description:
-          'Brave souls and aspiring adventurers, heed the call of Mount Olympus! Join our expedition to the mythical peak and explore breathtaking vistas, mysterious ruins, and maybe even meet the gods! Prepare for a once-in-a-lifetime adventure and the chance to carve your name into the annals of history. Are you up for the challenge?',
+          'Spartans, heed the call of Mount Olympus! Join our expedition to the mythical peak and explore breathtaking vistas, mysterious ruins, and maybe even meet the gods! Prepare for a once-in-a-lifetime adventure as we offer you the chance to carve your name into the annals of history. Are you, brave Spartans, up for the challenge?',
         announcer: 'Hermes',
         createdAt: ISODate('2023-10-10T12:30:00.000+0000'),
         updatedAt: ISODate('2023-10-10T12:30:00.000+0000'),
@@ -26,7 +26,7 @@ module.exports = {
         title:
           'Spartans, Seize Your Destiny - Stand Against the Forces of Persia!',
         description:
-          'Spartans, this is the hour to etch your name honorably into the annals of history! The invading forces of Persia, led by King Xerxes I, threaten the very heart of Greece. Do not falter, defenders of our land, embodiment of valor and strength. Rise as one, Spartans, and let the thunder of your war cry shake the earth! With unwavering resolve and unyielding determination, stand firm against the Persian invaders, for victory is the only path forward. Today, we declare that the spirit of Sparta shall shine brighter than ever before!',
+          'Spartans, this is the hour to etch your name honorably into the annals of history! The invading forces of Persia, led by King Xerxes I, threaten the very heart of Greece. Rise as one, Spartans, and let the thunder of your war cry shake the earth! With unwavering resolve and unyielding determination, stand firm against the Persian invaders, for victory is the only path forward. Today, we declare that the spirit of Sparta shall shine brighter than ever before!',
         announcer: 'Council of Spartan Elders',
         createdAt: ISODate('2023-09-16T09:00:00.000+0000'),
         updatedAt: ISODate('2023-09-16T09:00:00.000+0000'),
@@ -35,7 +35,7 @@ module.exports = {
       {
         title: 'Victory Over the Wolf Menace - Sparta Prevails!',
         description:
-          'Great news! The valiant warriors of Sparta have successfully defended our city against the relentless wolf attacks that threatened our people. Through their unwavering courage and steadfast determination, they have driven the wolf menace back into the wild. We celebrate the valor of our Spartan defenders and the safety of our city. Let this victory stand as a testament to the indomitable spirit of Sparta!',
+          'Valiant warriors of Sparta! You have successfully defended the city against the relentless wolf attacks that threatened our people. Through your unwavering courage and steadfast determination, you have driven the wolf menace back into the wild. We celebrate the safety of our city, let this victory stand as a testament to the indomitable spirit of Sparta!',
         announcer: 'Spartan Council',
         createdAt: ISODate('2023-09-25T08:15:00.000+0000'),
         updatedAt: ISODate('2023-09-25T08:15:00.000+0000'),
@@ -44,7 +44,7 @@ module.exports = {
       {
         title: 'Wolves Attack - Rally to Protect the City!',
         description:
-          'Urgent call to all citizens and brave defenders of our city! Packs of fierce wolves have launched a series of relentless attacks on our city, threatening the safety, security, and well-being of our people. We must stand together to protect our homes and loved ones. Join the defense effort, sharpen your blades, and ready your bows. Together, we shall drive back the wolf menace!',
+          'Urgent call to all Spartans! Packs of fierce wolves have launched a series of relentless attacks on our city, threatening the safety, security, and well-being of our people. We must stand together to protect our homes and loved ones, join the defense effort, sharpen your blades, and ready your shields!',
         announcer: 'City Council',
         createdAt: ISODate('2023-09-20T10:00:00.000+0000'),
         updatedAt: ISODate('2023-09-20T10:00:00.000+0000'),
