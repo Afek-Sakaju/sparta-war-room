4738ab5a975508feb6c29d8e3cfffd62209f7f26
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-06 12:19:12 +0300

    the prev implementation was'nt working, now added await before the promise call. works well

--- client/scripts/tactics-script.js
+++ client/scripts/tactics-script.js
@@ -2,7 +2,7 @@ window.onload = async () => {
   const isAuthenticated = await isAuthenticatedUser();
   if (!isAuthenticated) {
     // Avoids showing an alert to the user before the background image fully loads.
-    wait(0.5);
+    await wait(0.15);
     alert('Entrance permitted only to Spartans who have logged in.');
     window.location.href = '/login';
     localStorage.removeItem('jwtAccessToken');
