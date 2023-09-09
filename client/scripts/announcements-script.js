a1f511f7795f62fe54979b2f6bbf5e47a8840c0e
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-09 12:24:55 +0300

    improved code in the scripts of the ann and tactics pages to throw error message in case that a user is authenticated but the client can't fetch the data from the server

--- client/scripts/announcements-script.js
+++ client/scripts/announcements-script.js
@@ -17,36 +17,41 @@ window.onload = async () => {
   }
 
   updateNavbarAuthState(true);
-  unhideElements('announcements-panel');
+  try {
+    const annListContainer = document.getElementById('announcements-panel');
+    if (!annListContainer) return;
 
-  const annData = await getData('/announcements/all')
-    .then((d) => d?.json())
-    .catch((e) => console.error(e));
+    const annResponse = await getData('/announcements/all');
+    if (!annResponse) throw Error('There was an issue fetching tactics data.');
 
-  const annListContainer = document.getElementById('announcements-panel');
-  if (!annListContainer) return;
+    const annData = await annResponse?.json();
+    annData?.forEach(({ title, description, announcer }) => {
+      const annWrapper = document.createElement('div');
+      annWrapper.classList.add('announcement-container');
 
-  annData?.forEach(({ title, description, announcer }) => {
-    const annWrapper = document.createElement('div');
-    annWrapper.classList.add('announcement-container');
+      const annTitle = document.createElement('h3');
+      annTitle.classList.add('announcement-title');
+      annTitle.textContent = title;
+      annWrapper.appendChild(annTitle);
 
-    const annTitle = document.createElement('h3');
-    annTitle.classList.add('announcement-title');
-    annTitle.textContent = title;
-    annWrapper.appendChild(annTitle);
+      const annDescription = document.createElement('p');
+      annDescription.classList.add('announcement-text');
 
-    const annDescription = document.createElement('p');
-    annDescription.classList.add('announcement-text');
+      const replacedAnnDescription = addLineBreaksToText(description);
+      annDescription.textContent = replacedAnnDescription;
+      annWrapper.appendChild(annDescription);
 
-    const replacedAnnDescription = addLineBreaksToText(description);
-    annDescription.textContent = replacedAnnDescription;
-    annWrapper.appendChild(annDescription);
+      const annAnnouncer = document.createElement('p');
+      annAnnouncer.classList.add('announcement-text', 'announcer-text');
+      annAnnouncer.textContent = announcer;
+      annWrapper.appendChild(annAnnouncer);
 
-    const annAnnouncer = document.createElement('p');
-    annAnnouncer.classList.add('announcement-text', 'announcer-text');
-    annAnnouncer.textContent = announcer;
-    annWrapper.appendChild(annAnnouncer);
-
-    annListContainer.appendChild(annWrapper);
-  });
+      annListContainer.appendChild(annWrapper);
+      unhideElements('announcements-panel');
+    });
+  } catch (e) {
+    const message = e?.message ?? 'Server data retrieval issue.';
+    console.error(e);
+    await showAlert({ message });
+  }
 };
--- client/scripts/tactics-script.js
+++ client/scripts/tactics-script.js
@@ -17,37 +17,44 @@ window.onload = async () => {
   }
 
   updateNavbarAuthState(true);
-  unhideElements('tactics-panel');
+  try {
+    const tacticsListContainer = document.getElementById('tactics-panel');
+    if (!tacticsListContainer) return;
 
-  const tacticsData = await getData('/tactics/all')
-    .then((d) => d?.json())
-    .catch((e) => console.error(e));
+    const tacticsResponse = await getData('/tactics/all');
+    if (!tacticsResponse) {
+      throw Error('There was an issue fetching tactics data.');
+    }
 
-  const tacticsListContainer = document.getElementById('tactics-panel');
-  if (!tacticsListContainer) return;
+    const tacticsData = await tacticsResponse?.json();
+    tacticsData?.forEach(({ title, information, image }) => {
+      const tacticWrapper = document.createElement('div');
+      tacticWrapper.classList.add('tactic-container');
 
-  tacticsData?.forEach(({ title, information, image }) => {
-    const tacticWrapper = document.createElement('div');
-    tacticWrapper.classList.add('tactic-container');
+      const tacticTitle = document.createElement('h3');
+      tacticTitle.classList.add('tactic-title');
+      tacticTitle.textContent = title;
+      tacticWrapper.appendChild(tacticTitle);
 
-    const tacticTitle = document.createElement('h3');
-    tacticTitle.classList.add('tactic-title');
-    tacticTitle.textContent = title;
-    tacticWrapper.appendChild(tacticTitle);
+      const tacticImage = document.createElement('img');
+      tacticImage.classList.add('tactic-image');
+      tacticImage.src = `data:image/jpeg;base64,${image}`;
+      tacticImage.alt = title;
+      tacticWrapper.appendChild(tacticImage);
 
-    const tacticImage = document.createElement('img');
-    tacticImage.classList.add('tactic-image');
-    tacticImage.src = `data:image/jpeg;base64,${image}`;
-    tacticImage.alt = title;
-    tacticWrapper.appendChild(tacticImage);
+      const tacticDescription = document.createElement('p');
+      tacticDescription.classList.add('tactic-text');
 
-    const tacticDescription = document.createElement('p');
-    tacticDescription.classList.add('tactic-text');
+      const replacedTacticInformation = addLineBreaksToText(information, 2);
+      tacticDescription.textContent = replacedTacticInformation;
+      tacticWrapper.appendChild(tacticDescription);
 
-    const replacedTacticInformation = addLineBreaksToText(information, 2);
-    tacticDescription.textContent = replacedTacticInformation;
-    tacticWrapper.appendChild(tacticDescription);
-
-    tacticsListContainer.appendChild(tacticWrapper);
-  });
+      tacticsListContainer.appendChild(tacticWrapper);
+      unhideElements('tactics-panel');
+    });
+  } catch (e) {
+    const message = e?.message ?? 'Server data retrieval issue.';
+    console.error(e);
+    await showAlert({ message });
+  }
 };
